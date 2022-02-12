815ef1ae0f870fef69ae410ff68dfbc1d92fb064
Rahul Singh <rstraining4@gmail.com>
2022-02-12 19:32:09 +0000

    newcode

--- ECS-services.tf
+++ ECS-services.tf
@@ -14,8 +14,10 @@ resource "aws_ecs_service" "service" {
     target_group_arn     = "${aws_alb_target_group.ecs-target-group.arn}"         # attaching load_balancer target group to ecs
  }
   network_configuration {
-    security_groups       = var.default_security_group 
-    subnets               = var.default_subnet  ## Enter the private subnet id
+    #security_groups       = var.default_security_group 
+    #subnets               = var.default_subnet
+    security_groups       = ["sg-03c4e631d072f58ed"] 
+    subnets               = ["subnet-0e4a3c689e1b7eb26"]
     assign_public_ip      = "false"
   }
   depends_on              = ["aws_alb_listener.ecsapp"]
