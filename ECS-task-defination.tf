2e589023de0cc5371328895bf414f6f1066000c0
Rahul Singh <rstraining4@gmail.com>
2022-02-12 19:25:02 +0000

    newcode

--- ECS-task-defination.tf
+++ ECS-task-defination.tf
@@ -4,13 +4,13 @@
 
 resource "aws_ecs_task_definition" "task_definition" {
   container_definitions    = "${data.template_file.task_definition.rendered}"                                         # task defination json file location
-  execution_role_arn       = "${aws_iam_role.ecs-instance-role.id}"                                                                  # role for executing task
+  execution_role_arn       = aws_iam_role.ecs-instance-role.arn                                                                # role for executing task
   family                   = "openapi-task-defination"                                                                      # task name
   network_mode             = "awsvpc"                                                                                      # network mode awsvpc, brigde
   memory                   = "2048"
   cpu                      = "1024"
   requires_compatibilities = ["EC2"]                                                                                       # Fargate or EC2
-  task_role_arn            = "${aws_iam_role.ecs-service-role.name}"                                                                     # TASK running role
+  task_role_arn            = aws_iam_role.ecs-instance-role.arn                                                                      # TASK running role
 } 
 
 data "template_file" "task_definition" {
