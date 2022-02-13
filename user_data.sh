1913e8040a772e03742fe8ab71110cb5cab9d4e5
Rahul Singh <rstraining4@gmail.com>
2022-02-13 18:21:46 +0000

    first

--- user_data.sh
+++ user_data.sh
@@ -5,10 +5,14 @@
 sudo yum update -y
 sudo amazon-linux-extras disable docker
 sudo amazon-linux-extras install -y ecs
-sudo systemctl enable --now ecs
-curl -s http://localhost:51678/v1/metadata | python -mjson.tool
-#sudo service docker start
-#sudo start ecs
+systemctl enable --now --no-block ecs.service
+sudo cp /usr/lib/systemd/system/ecs.service /etc/systemd/system/ecs.service
+sudo sed -i '/After=cloud-final.service/d' /etc/systemd/system/ecs.service
+sudo systemctl daemon-reload
+sudo curl -s http://localhost:51678/v1/metadata | python -mjson.tool
+
+#There is deadlock so we need to modify the service
+#https://github.com/aws/amazon-ecs-agent/issues/1707
 
 #Adding cluster name in ecs config
 echo ECS_CLUSTER=ecs-ec2-cluster >> /etc/ecs/ecs.config
