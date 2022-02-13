120450c4a078d5fa5d36db51107c403fc9ed8f38
Rahul Singh <rstraining4@gmail.com>
2022-02-13 17:05:47 +0000

    first

--- user_data.sh
+++ user_data.sh
@@ -3,9 +3,11 @@
 # Update all packages
 
 sudo yum update -y
-sudo amazon-linux-extras install ecs
-sudo service docker start
-sudo start ecs
+sudo amazon-linux-extras disable docker
+sudo amazon-linux-extras install -y ecs; sudo systemctl enable --now ecs
+curl -s http://localhost:51678/v1/metadata | python -mjson.tool
+#sudo service docker start
+#sudo start ecs
 
 #Adding cluster name in ecs config
 echo ECS_CLUSTER=ecs-ec2-cluster >> /etc/ecs/ecs.config
