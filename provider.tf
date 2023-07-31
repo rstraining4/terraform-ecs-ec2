93f2e6d042f4f5a5e3067d2716681903beb36e27
Rahul <Rahul@RAHUL>
2023-07-31 13:06:11 +0100

    Jenkinsfile update

--- provider.tf
+++ provider.tf
@@ -13,5 +13,5 @@ provider "aws" {
   region     = var.aws_region
   #if you are running from AWS ec2 linux instance please use bellow credentials section
   shared_credentials_file = "$HOME/.aws/credentials"
-  profile = "MyAWS"
+  profile = "default"
 }
