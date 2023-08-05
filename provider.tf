3f1aba81e96de5ce1015e74478f567cda159f3a9
Rahul <Rahul@RAHUL>
2023-08-05 14:24:18 +0100

    Jenkinsfile update

--- provider.tf
+++ provider.tf
@@ -1,17 +1,30 @@
+#terraform {
+#  required_providers {
+#    aws = {
+#      source  = "hashicorp/aws"
+#      version = "= 3.76.1"
+#    }
+#  }
+#}
+#
+#provider "aws" {
+#  #access_key = "put-your-access-key"
+#  #secret_key = "put-your-secret-key"
+#  region     = var.aws_region
+#  #if you are running from AWS ec2 linux instance please use bellow credentials section
+#  #shared_credentials_file = "$HOME/.aws/credentials"
+#  profile = "MyAWS"
+#}
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "= 3.76.1"
+      version = "~> 5.0"
     }
   }
 }
 
+# Configure the AWS Provider
 provider "aws" {
-  #access_key = "put-your-access-key"
-  #secret_key = "put-your-secret-key"
-  region     = var.aws_region
-  #if you are running from AWS ec2 linux instance please use bellow credentials section
-  #shared_credentials_file = "$HOME/.aws/credentials"
-  profile = "MyAWS"
+  region = "ap-south-1"
 }
